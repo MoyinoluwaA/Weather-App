67715efc71bb20accebc91b6537ee07e35e658e5
moyinoluwaa <dayoh14@gmail.com>
2024-03-21 16:50:59 +0100

    feature: react native weather app

---
+++ README.md
@@ -0,0 +1,18 @@
+# Weather App
+Weather app made by following Free Code Camp course on React Native. Built with Expo and react navigation.
+Weather data is gotten from the openweathermap [api](https://openweathermap.org/api).
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm start
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/hooks/useGetWeather.js
@@ -0,0 +1,42 @@
+import { useState, useEffect, useCallback } from 'react'
+import * as Location from 'expo-location'
+import { WEATHER_API_KEY } from '@env'
+
+export const useGetWeather = () => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [weather, setWeather] = useState([])
+  const [lat, setLat] = useState([])
+  const [lon, setLon] = useState([])
+
+  const fetchWeatherData = useCallback(async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      )
+      const data = await res.json()
+      setWeather(data)
+    } catch (err) {
+      setError('could not fetch weather')
+    } finally {
+      setLoading(false)
+    }
+  }, [lat, lon])
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLat(location.coords.latitude)
+      setLon(location.coords.longitude)
+      await fetchWeatherData()
+    })()
+  }, [lat, lon, fetchWeatherData])
+
+  return [loading, error, weather]
+}
